38c4026a99c7cc510cbccbfa445b7ab44e6cf863
remsysc <raipam44@gmail.com>
2025-08-19 20:19:00 +0800

    init

---
+++ src/main/java/com/sysc/bulag_faust/BulagFaustApplication.java
@@ -0,0 +1,13 @@
+package com.sysc.bulag_faust;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BulagFaustApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BulagFaustApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sysc/bulag_faust/BulagFaustApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sysc.bulag_faust;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BulagFaustApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
